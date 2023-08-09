478aa78d4146a5dcda859242993fc4cc5d97ed01
Julio Torres <jtorres990@gmail.com>
2023-08-09 12:27:35 -0500

    feat(doc): add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+      .setTitle('Drone')
+      .setDescription('The Drone API description')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+
+    app.enableCors({
+      origin: process.env.ALLOW_ORIGIN,
+    });
+    await app.listen(parseInt(process.env.PORT, 10) || 3000);
+  }
+  bootstrap();
 }
 bootstrap();
